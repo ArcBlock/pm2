0bb0d72453aa1a741046f6c7db8b25c3e5604253
JianChao <guhuo13@gmail.com>
2025-07-23 21:19:02 +0800

    feat: support memory snapshot v3

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -1,4 +1,6 @@
 /* eslint-disable no-console */
+
+// 如果 pm2 update 无法执行完成，那么可能是 node_modules 需要重装一下
 const pm2Path = path.join(process.cwd(), 'bin', 'pm2')
 const pm2DevPath = path.join(process.cwd(), 'bin', 'pm2-dev')
 await $`echo "alias p='${pm2Path}'" >> ~/.zshrc`
